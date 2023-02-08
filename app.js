adb6c29105f310dab90ccd000a9d3094ca498ac6
Emeamos <Emediongamos.engr@gmail.com>
2023-02-08 17:20:50 +0100

    remove port from app.js

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.use("/api/v1/task", taskRoute);
 //middleware
 
 
-const PORT = process.env.Port || 7007;
+const PORT = process.env.PORT;
 app.listen(PORT,console.log(`Server is running at ${PORT}`))
