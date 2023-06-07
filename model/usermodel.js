96412bb44b7f50863d18cd1ef2f3b1832c14d537
Emeamos <Emediongamos.engr@gmail.com>
2023-06-07 08:49:46 +0100

    updating user model

--- model/usermodel.js
+++ model/usermodel.js
@@ -14,6 +14,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         required:[true, "password is needed"]
     },
+    essential: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "essential",
+        },
+      ],
 
 })
 
