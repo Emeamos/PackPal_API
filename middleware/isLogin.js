c8322041fb02faaf481f841f283284e625e046f5
Emeamos <Emediongamos.engr@gmail.com>
2023-06-08 11:16:57 +0100

    fixing bud on Login middeware

--- middleware/isLogin.js
+++ middleware/isLogin.js
@@ -2,21 +2,21 @@
 import {obtainTokenFromHeader} from "../util/obtaintokenfromheader.js";
 import { verifytoken } from "../util/verifytoken.js";
 
-
 export const isLogin = (req, res, next) => {
     // get token from header
     const token = obtainTokenFromHeader(req);
-    
+  
+    // Verify the token and obtain the userDecoded object
     const userDecoded = verifytoken(token);
-
-    req.userAuth = userDecoded.id;
-
-    if(!userDecoded){
-        return res.json({
-            status: "failed",
-            message: "Kindly, login in because, it seem the token is either expired or invalid"
-        })
-    }else{
-        next();
+  
+    if (!userDecoded) {
+      return res.status(401).json({
+        status: "failed",
+        message: "Kindly, login because the token is either expired or invalid",
+      });
     }
-}
+  
+    req.userAuth = userDecoded.id;
+  
+    next();
+  };
